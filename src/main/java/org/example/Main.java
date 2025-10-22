f10ae7a1885a0c2ab381d97a43cb51198a8c1d8a
Matteo Galvagni <50049534+galvagnimatteo@users.noreply.github.com>
2025-10-22 15:51:36 +0200

    feat: removed comment (#29)

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,9 +7,5 @@ public class Main {
     //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
     // to see how IntelliJ IDEA suggests fixing it.
     System.out.printf("Hello and welcome!");
-
-    // Removed the default for loop.
-    // This is a critical feature to enhance synergetic connections across the team.
-    // TODO we could schedule a team building experience based on this incredibly effective feature.
   }
 }
